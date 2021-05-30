874530034ea04bab00c5354bd6700645cd1c235a
Yarden <yarden@bis-tec.co.il>
2021-05-30 12:30:13 +0300

    add comments

--- controllers/messages.js/messages.js
+++ controllers/messages.js/messages.js
@@ -1,29 +1,27 @@
 const messagesRepository = require('./messages.rep');
 const responseService = require('../../utils/response');
 
-module.exports = (app)=> {
+module.exports = (app) => {
     // add new message by topic and publish messages to topic listeners
     app.post('/new/message', (req, res) => {
-        try { 
+        try {
             const { topic, message } = req.body;
             messagesRepository.newMessage(topic, message);
             res.status(responseService.status.OK).send(responseService.message.SUCCESS)
         } catch (err) {
-            console.log(err) 
             res.status(responseService.status.INTERNAL_ERROR).send(JSON.stringify(err));
         }
     })
 
-    // clientId subscribe to topic. Deliver all messages sent to that topic 
+    // clientId subscribe to topic  
     app.get('/subscribe/:clientId/:topic', (req, res) => {
         try {
             const { clientId, topic } = req.params;
             messagesRepository.clientSubscribe(clientId, topic, res, req);
-        } catch (err) { 
-            console.log(err)
+        } catch (err) {
             res.status(responseService.status.INTERNAL_ERROR).send(JSON.stringify(err));
         }
-    }) 
+    })
 
     // unsubscribe clientId from spesific topic
     app.get('/unsubscribe/:clientId/:topic', (req, res) => {
@@ -31,7 +29,6 @@ module.exports = (app)=> {
             const { clientId, topic } = req.params;
             messagesRepository.clientUnsubscribe(clientId, topic);
             res.status(responseService.status.OK).send(responseService.message.SUCCESS)
-
         } catch (err) {
             res.status(responseService.status.INTERNAL_ERROR).send(JSON.stringify(err));
         }
--- controllers/messages.js/messages.rep.js
+++ controllers/messages.js/messages.rep.js
@@ -9,45 +9,48 @@ module.exports = {
     clientUnsubscribe
 }
 
-/*if there is a clients that subscribe to this topic add the msg to messages in clientMap
-else save the msg in messagesMap by topic*/
+/*save the msg in messagesMap by topic 
+if there is a clients that subscribe to this topic add the msg to messages in clientMap*/
 function newMessage(topic, message) {
     try {
         if (clientsMap.has(topic)) {
             clientsMap.get(topic).forEach(client => {
                 client.messages.push(message)
             })
-        } else {
-            if (!messagesMap.has(topic)) {
-                messagesMap.set(topic, [message])
-            } else {
-                const prevMessages = messagesMap.get(topic);
-                prevMessages.push(message);
-                messagesMap.set(topic, prevMessages);
-            }
         }
+        if (!messagesMap.has(topic)) {
+            messagesMap.set(topic, [message])
+        } else {
+            const prevMessages = messagesMap.get(topic);
+            prevMessages.push(message);
+            messagesMap.set(topic, prevMessages);
+        }
+
         publish(topic);
     } catch (err) {
         throw err;
     }
 }
 
-/*if the subscribed clientId have messages in the spesific topic we send immediately receive the messages
-else save the rellavant data in clientData*/ 
+/*save client res&messages&clientId in clientMap bt topic
+if the subscribed clientId have messages in the spesific topic send immediately the messages*/
 function clientSubscribe(clientId, topic, res, req) {
     try {
         const messages = messagesMap.has(topic) ? messagesMap.get(topic) : [];
-        if (messages.length > 0) {
-            res.end(JSON.stringify({ messages, topic }));
+
+        if (!clientsMap.has(topic)) {
+            clientsMap.set(topic, [{ clientId: clientId, res: res, messages: messages }])
+            deliverAllMessages(messages, topic, clientId);
         } else {
-            if (!clientsMap.has(topic)) {
-                clientsMap.set(topic, [{ clientId: clientId, res: res, messages: messages }])
-            } else {
-                const clientData = clientsMap.get(topic);
-                if (!clientData.find(x => x.clientId == clientId)) {
-                    clientData.push({ clientId: clientId, res: res, messages: messages });
-                }
+            const clientData = clientsMap.get(topic);
+            const index = clientData.findIndex(x => x.clientId == clientId);
+            if (index == -1) {
+                clientData.push({ clientId: clientId, res: res, messages: messages });
                 clientsMap.set(topic, clientData);
+                deliverAllMessages(messages, topic, clientId);
+            } else {
+                clientData[index].res = res;
+                deliverAllMessages(clientData[index].messages, topic, clientId);
             }
         }
     } catch (err) {
@@ -55,7 +58,22 @@ function clientSubscribe(clientId, topic, res, req) {
     }
 }
 
-/*delete client from clientData array by topic*/ 
+/*send to spesific client all his messages and initial the client data*/
+function deliverAllMessages(messages, topic, clientId) {
+    try {
+        if (messages.length > 0) {
+            const clientData = clientsMap.get(topic);
+            const client = clientData.find(x => x.clientId == clientId);
+            client.res.end(JSON.stringify({ messages, topic }))
+            client.res = null;
+            client.messages = [];
+        }
+    } catch (err) {
+        throw err;
+    }
+}
+
+/*delete client from clientsMap by topic*/
 function clientUnsubscribe(clientId, topic) {
     try {
         if (clientsMap.has(topic)) {
@@ -71,20 +89,22 @@ function clientUnsubscribe(clientId, topic) {
     }
 }
 
-/*if topic exist in client map send to all his clients the messages and then unsubscribed*/
+/*if topic exist in client map send to all his clients the messages*/
 function publish(topic) {
-    if (clientsMap.has(topic)) {
-        const clientData = clientsMap.get(topic);
-        let savedClientsId = [];
+    try {
+        if (clientsMap.has(topic)) {
+            const clientData = clientsMap.get(topic);
 
-        clientData.forEach(element => {
-            const result = { messages: element.messages, topic: topic };
-            element.res.end(JSON.stringify(result));
-            savedClientsId.push(element.clientId);
-        });
-
-        savedClientsId.forEach(clientId => {
-            clientUnsubscribe(clientId, topic);
-        })
+            clientData.forEach(element => {
+                if (element.res) {
+                    const result = { messages: element.messages, topic: topic };
+                    element.res.end(JSON.stringify(result));
+                    element.res = null;
+                    element.messages = [];
+                }
+            });
+        }
+    } catch (err) {
+        throw err;
     }
 }
